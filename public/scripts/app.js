21a63763f988eb673d75aaca4bfb4fd34ffacacb
Anna Sobolewska <sobolewska.anna@gmail.com>
2016-04-02 22:09:13 -0700

    added handlebars to profile results

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,24 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
+  var profilesSource   = $("#profiles-template").html();
+  var profilesTemplate = Handlebars.compile(profilesSource);
 
-// your code
+  $.ajax({
+    method: 'GET',
+    url: '/profile',
+    dataType: 'json',
+    success: onProfileSuccess,
+    error: onError
+  });
+
+  function onProfileSuccess(data) {
+    var profilesHTML = profilesTemplate({profiles: data});
+    $('#profiles').append(profilesHTML);
+  }
+
+  function onError(error) {
+    console.log('didn\'t find shit');
+  }
 
 });
